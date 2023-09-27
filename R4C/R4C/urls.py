ff24217b6920b15be19eda1802f010235ec891ec
exites <or.strangelove@gmail.com>
2023-09-27 22:08:37 +0300

    task2

--- R4C/R4C/urls.py
+++ R4C/R4C/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from robots import views
+
 urlpatterns = [
     path('', include('robots.urls')),
     path('admin/', admin.site.urls),
+    path('generate_excel_summary/', views.generate_excel_summary, name='generate_excel_summary'),
 ]
 
 if settings.DEBUG:
--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -3,8 +3,18 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
+from django.shortcuts import render
+
+
+import os
+import openpyxl
+from django.conf import settings
+from openpyxl.utils.dataframe import dataframe_to_rows
+import pandas as pd
+from datetime import datetime, timedelta
+from django.db.models import Count
 from .models import Robot
-from datetime import datetime
+from django.http import HttpResponse
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -35,3 +45,50 @@ class RobotAPIView(View):
 
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data.'}, status=400)
+
+
+def generate_excel_summary(request):
+    # Получение текущей даты
+    today = datetime.today().strftime('%Y-%m-%d')
+
+    # Формирование пути к файлу Excel
+    file_name = f'summary_{today}.xlsx'
+    excel_file_path = os.path.join(settings.STATIC_ROOT, file_name)
+
+    # Получение данных о количестве роботов для каждой модели и версии за последнюю неделю
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=7)
+    robot_data = Robot.objects.filter(created__range=(start_date, end_date)) \
+        .values('model', 'version') \
+        .annotate(quantity=Count('id'))
+
+    # Создание Excel-файла
+    wb = openpyxl.Workbook()
+
+    # Создание страниц для каждой модели
+    unique_models = robot_data.values_list('model', flat=True).distinct()
+    for model in unique_models:
+        ws = wb.create_sheet(title=model)
+        df = pd.DataFrame(robot_data.filter(model=model))
+        for r_idx, row in enumerate(dataframe_to_rows(df, index=False, header=True), 1):
+            for c_idx, value in enumerate(row, 1):
+                cell = ws.cell(row=r_idx, column=c_idx, value=value)
+
+    # Удаление изначальной пустой страницы
+    del wb[wb.sheetnames[0]]
+
+    # Сохранение файла
+    wb.save(excel_file_path)
+
+    # Открытие и чтение файла в бинарном режиме
+    with open(excel_file_path, 'rb') as excel_file:
+        response = HttpResponse(excel_file.read(),
+                                content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = f'attachment; filename={file_name}'
+        return response
+
+
+
+
+def index_view(request):
+    return render(request, 'robots/index.html')
